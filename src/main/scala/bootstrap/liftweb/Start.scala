c13d0c5a55f5e291d039157103b84cf46bf4b7af
Joe Barnes <barnesjd@gmail.com>
2015-03-06 16:11:51 -0600

    Reading  in production mode

--- src/main/scala/bootstrap/liftweb/Start.scala
+++ src/main/scala/bootstrap/liftweb/Start.scala
@@ -7,35 +7,40 @@ import org.eclipse.jetty.webapp.WebAppContext
 
 object Start extends App with Loggable {
 
-	LoggingAutoConfigurer().apply()
+  LoggingAutoConfigurer().apply()
 
-	logger.info("run.mode: " + Props.modeName)
-	logger.trace("system environment: " + sys.env)
-	logger.trace("system props: " + sys.props)
-	logger.info("liftweb props: " + Props.props)
-	logger.info("args: " + args.toList)
+  logger.info("run.mode: " + Props.modeName)
+  logger.trace("system environment: " + sys.env)
+  logger.trace("system props: " + sys.props)
+  logger.info("liftweb props: " + Props.props)
+  logger.info("args: " + args.toList)
 
-	startLift()
+  startLift()
+  
+  def startLift(): Unit = {
+    logger.info("starting Lift server")
 
-	def startLift(): Unit = {
-		logger.info("starting Lift server")
+    val port = {
+      val prop = Props.get("jetty.port", "8080")
+      val str = if(prop startsWith "$") System.getenv(prop substring 1) else prop
+      str.toInt
+    }
 
-		val port: Int = Props.getInt("jetty.port").openOrThrowException("cannot find jetty.port configuration value")
-		logger.info(s"port number is $port")
+    logger.info(s"port number is $port")
 
-		val webappDir: String = Option(this.getClass.getClassLoader.getResource("webapp"))
-			.map(_.toExternalForm)
-			.filter(_.contains("jar:file:")) // this is a hack to distinguish in-jar mode from "expanded"
-			.getOrElse("src/main/webapp")
+    val webappDir: String = Option(this.getClass.getClassLoader.getResource("webapp"))
+      .map(_.toExternalForm)
+      .filter(_.contains("jar:file:")) // this is a hack to distinguish in-jar mode from "expanded"
+      .getOrElse("src/main/webapp")
 
-		logger.info(s"webappDir: $webappDir")
+    logger.info(s"webappDir: $webappDir")
 
-		val server = new Server(port)
-		val context = new WebAppContext(webappDir, Props.get("jetty.contextPath").openOr("/"))
-		server.setHandler(context)
-		server.start()
-		logger.info(s"Lift server started on port $port")
-		server.join()
-	}
+    val server = new Server(port)
+    val context = new WebAppContext(webappDir, Props.get("jetty.contextPath").openOr("/"))
+    server.setHandler(context)
+    server.start()
+    logger.info(s"Lift server started on port $port")
+    server.join()
+  }
 
 }
