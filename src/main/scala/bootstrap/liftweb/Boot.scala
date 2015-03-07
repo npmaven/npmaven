ec8a9f2fac5b51c99ad877b86d59e8de3eea5777
Joe Barnes <barnesjd@gmail.com>
2015-03-06 22:34:22 -0600

    Cleaned up Boot stuff we aren't using

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -2,20 +2,13 @@ package bootstrap.liftweb
 
 import java.io.InputStream
 
-import net.liftweb._
 import org.npmaven.rest.NpmRest
 import org.npmaven.snippet.Pamflet
-import util._
-import Helpers._
 
-import common._
-import net.liftweb.http._
+import net.liftweb._
+import http._
 import sitemap._
 import Loc._
-import net.liftmodules.JQueryModule
-import net.liftweb.http.js.jquery._
-
-import scala.xml.NodeSeq
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -29,29 +22,15 @@ class Boot {
     // Build SiteMap
     val entries = List(
       // Allows everything in /site (created by Pamflet) to be exposed.
-      Menu(Loc("npmaven", new Link(List("site"), true), "npmaven")
-      )
+      Menu(Loc("npmaven", new Link(List("site"), true), "npmaven"))
     )
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
 
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
-
-    //Init the jQuery module, see http://liftweb.net/jquery for more information.
-    LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery191
-    JQueryModule.init()
 
     // For fixing the +'s in Pamflet's URLs
     LiftRules.contentParsers = List(
