6cacf1351064a291fe27af56845b63190f4026cf
Joe Barnes <barnesjd@gmail.com>
2015-03-07 21:03:35 -0600

    Implemented a sha1 sum function

--- src/main/scala/org/npmaven/artifacts/Artifactory.scala
+++ src/main/scala/org/npmaven/artifacts/Artifactory.scala
@@ -1,6 +1,8 @@
 package org.npmaven
 package artifacts
 
+import java.security.MessageDigest
+
 import model._
 
 import scala.xml.Node
@@ -15,5 +17,11 @@ object Artifactory {
       <artifactId>{name}</artifactId>
       <version>{version}</version>
     </project>
+  }
+
+  def sha1(bytes:Array[Byte]):String = {
+    val md = MessageDigest.getInstance("SHA-1")
+    val digest = md digest bytes
+    digest.map(b => String.format("%02x", b:java.lang.Byte)).mkString
   }
 }
---
+++ src/test/scala/org/npmaven/artifacts/ArtifactorySha1Specs.scala
@@ -0,0 +1,18 @@
+package org.npmaven
+package artifacts
+
+import net.liftweb.common.Full
+import net.liftweb.http.LiftRules
+import util.Data._
+import org.specs2.mutable.Specification
+
+object ArtifactorySha1Specs extends Specification {
+  "Artifactory.sha1" should {
+    "correctly calculate the sha1 of our canned angular-1.3.13.jar" in {
+      val bytes = LiftRules.loadResource("/org/npmaven/artifacts/angular-1.3.13.jar")
+      val sha1 = bytes.map(Artifactory.sha1)
+
+      sha1 should be equalTo(Full("09de1e0dcf0b0873bcc58dffa5a6595121fb3cef"))
+    }
+  }
+}
