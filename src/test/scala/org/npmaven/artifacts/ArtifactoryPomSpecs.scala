499add6d12e39f9df90cbe0c1bd32f542cc19941
Joe Barnes <barnesjd@gmail.com>
2015-03-07 20:38:09 -0600

    Renaming ArtifactorySpecs so we can have several Specs for each function

--- src/test/scala/org/npmaven/artifacts/ArtifactoryPomSpecs.scala
+++ src/test/scala/org/npmaven/artifacts/ArtifactoryPomSpecs.scala
@@ -6,8 +6,8 @@ import model._
 import org.specs2.matcher.XmlMatchers
 import org.specs2.mutable.Specification
 
-object ArtifactorySpecs extends Specification with XmlMatchers {
-  "Artifactory object" should {
+object ArtifactoryPomSpecs extends Specification with XmlMatchers {
+  "Artifactory.pom" should {
     "produce a minimal pom" in {
       val pkg = Package("angular", "1.3.14")
       val pom =
