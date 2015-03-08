2b130beca53160fe4d69febc8818fbede30e483f
Joe Barnes <barnesjd@gmail.com>
2015-03-08 16:25:25 -0500

    Downloads a jar with the manifest and org/npmaven dirs

--- src/main/scala/org/npmaven/artifacts/Artifactory.scala
+++ src/main/scala/org/npmaven/artifacts/Artifactory.scala
@@ -1,11 +1,18 @@
 package org.npmaven
 package artifacts
 
-import java.security.MessageDigest
+import java.io.{OutputStreamWriter, PrintWriter, ByteArrayOutputStream}
 
 import model._
 
+import java.security.MessageDigest
+import java.util.jar._
+
+import scala.collection.immutable.ListMap
+import scala.concurrent.{ExecutionContext, Future}
 import scala.xml.Node
+
+import dispatch._
 
 object Artifactory {
   def pom(pkg:Package):Node = {
@@ -23,5 +30,55 @@ object Artifactory {
     val md = MessageDigest.getInstance("SHA-1")
     val digest = md digest bytes
     digest.map(b => String.format("%02x", b:java.lang.Byte)).mkString
+  }
+
+  def jar(pkg:Package)(implicit ec:ExecutionContext):Future[Array[Byte]] = {
+    pkg.dist.map { dist =>
+      val req = url(dist.tarball).GET
+      Http(req OK as.Bytes).map(b => pkgToJar(pkg, b))
+    }.getOrElse(Future.failed(new Exception("Package did not contain a dist.tarball value")))
+  }
+
+  private def manifest(pkg:Package):ListMap[String, String] =
+    ListMap(
+      "Manifest-Version" -> "1.0",
+      "Implementation-Vendor" -> "org.npmaven",
+      "Implementation-Title" -> pkg.name,
+      "Implementation-Version" -> pkg.version,
+      "Implementation-Vendor-Id" -> "org.npmaven",
+      "Specification-Vendor" -> "org.npmaven",
+      "Specification-Title" -> pkg.name,
+      "Implementation-URL" -> "http://repo.npmaven.org/",
+      "Specification-Version" -> pkg.version
+    )
+
+  private def manifestFile(entries:ListMap[String, String]):List[String] =
+    entries.map{ case (k, v) => k+": "+v }.toList
+
+  private def writeManifest(mf:List[String], jar:JarOutputStream):Unit = {
+    jar.putNextEntry(new JarEntry("META-INF/"))
+    jar.putNextEntry(new JarEntry("META-INF/MANIFEST.MF"))
+    val pStream = new PrintWriter(new OutputStreamWriter(jar, "ISO-8859-1"))
+    val print = pStream.println(_:String)
+    mf foreach print
+    pStream.flush()
+    // Do not close the pStream, as it will close the jar
+  }
+
+  private def mf(pkg:Package, jar:JarOutputStream):Unit = {
+    val entries = manifest(pkg)
+    val lines   = manifestFile(entries)
+    writeManifest(lines, jar)
+  }
+
+  private def pkgToJar(pkg:Package, content:Array[Byte]):Array[Byte] = {
+    val out = new ByteArrayOutputStream()
+    val jar = new JarOutputStream(out)
+    mf(pkg, jar)
+    jar.putNextEntry(new JarEntry("org/"))
+    jar.putNextEntry(new JarEntry("org/npmaven/"))
+    jar.flush()
+    jar.close()
+    out.toByteArray
   }
 }
--- src/main/scala/org/npmaven/rest/NpmRest.scala
+++ src/main/scala/org/npmaven/rest/NpmRest.scala
@@ -1,6 +1,8 @@
 package org.npmaven
 package rest
 
+import java.io.OutputStream
+
 import model._
 import commonjs. {Registry => NpmRegistry}
 import util.FutureConversions._
@@ -10,31 +12,37 @@ import net.liftweb.common.Loggable
 import net.liftweb.http._
 import net.liftweb.http.rest.RestHelper
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object NpmRest extends RestHelper with Loggable {
   lazy val npm = new NpmRegistry("registry.npmjs.org/")
 
-  def toResponse(pkg:Package, art:Artifact):LiftResponse = art match {
-    case Pom => XmlResponse(pom(pkg))
+  def toResponse(pkg:Package, art:Artifact):Future[LiftResponse] = art match {
+    case Pom => Future.successful(XmlResponse(pom(pkg)))
+    case Jar => jar(pkg).map(b => OutputStreamResponse((out: OutputStream) => out write b))
     case Sha1(Pom) => {
       val sum = sha1(XmlResponse(pom(pkg)).toResponse.data)
-      PlainTextResponse(sum, 200)
+      Future.successful(PlainTextResponse(sum, 200))
     }
-    case _ => NotFoundResponse()
+    case _ => Future.successful(NotFoundResponse())
   }
 
   serve {
-    case "repo" :: "npm" :: "org" :: "npmaven" :: name :: version :: artifact :: Nil XmlReq _ => {
+    case "repo" :: "npm" :: "org" :: "npmaven" :: name :: version :: artifact :: Nil RestReq _ => {
       val pkg = Package(name, version)
       val art = Artifact(artifact)
       logger.trace(S.request)
 
       val f = npm.get(pkg)
-        .map(p => art.map(a => toResponse(p, a)).openOr(NotFoundResponse()))
+        .flatMap(p => art.map(a => toResponse(p, a)).openOr(Future.successful(NotFoundResponse())))
         .recover{case e:Exception => logger.trace(e); NotFoundResponse()}
         .la
 
       f
     }
+  }
+
+  private object RestReq {
+    def unapply(r: Req): Option[(List[String], Req)] = Some(r.path.partPath -> r)
   }
 }
